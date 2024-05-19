a6b96bf4735fb3f89e3f0fcace88974eddfb2f68
michellemej22596 <mej22596@uvg.edu.gt>
2024-05-18 23:01:20 -0600

    Feat: Programa Python de Cadenas

--- cadenasTP.py
+++ cadenasTP.py
@@ -1,15 +1,26 @@
 import numpy as np
 
 def monty_hall_markov(trials=10000):
+    # Estados: 0 - Puerta con premio, 1-9 Puertas sin premio
     wins_change = 0
     wins_stay = 0
+    transition_matrix = np.array([[0, 1], [1, 0]])  # Cambiar o quedarse
+
     for _ in range(trials):
         prize_door = np.random.randint(0, 10)
         chosen_door = np.random.randint(0, 10)
+        
         if chosen_door == prize_door:
-            wins_stay += 1
+            state = 0  # Estado ganador
         else:
+            state = 1  # Estado perdedor
+
+        final_state = np.random.choice([0, 1], p=transition_matrix[state])
+        if final_state == 0:
             wins_change += 1
+        else:
+            wins_stay += 1
+            
     return wins_stay, wins_change
 
 trials = 10000
